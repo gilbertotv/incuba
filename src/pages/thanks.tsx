db72e24561fa07ca76f6052cec494d7bcfbcc3ee
gilbertotv <gilcarrillo7@gmail.com>
2022-05-18 23:28:41 -0500

    font size

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -6,12 +6,12 @@ const Thanks = () => {
 	return (
 		<Layout title="Gracias">
 			<Section>
-				<p className="mt-28 sm:mt-8 text-3xl sm:text-4xl text-center mb-8">
+				<p className="mt-36 sm:mt-8 text-lg sm:text-2xl text-center mb-8">
 					Con tu donativo estás contribuyendo al desarrollo del espacio cívico
 					en México. Cada aporte que recibimos permite que organizaciones
 					continuen trabajando en sus comunidades.
 				</p>
-				<p className="text-3xl sm:text-4xl text-center">
+				<p className="text-lg sm:text-2xl text-center">
 					Para solicitar tu recibo fiscal, escribe a{" "}
 					<a className="text-orange1" href="mailto:admon@incuba.ong">admon@incuba.ong</a>
 				</p>
